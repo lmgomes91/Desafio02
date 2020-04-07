b01bfecdbc16abe9853112442f36ad3822742be5
LucasGomes <lgomes@post.com>
2020-04-07 00:59:00 -0300

    Versao final, aprovado em todos os testes automatizados

--- src/app.js
+++ src/app.js
@@ -91,7 +91,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   repositories[repositoryIndex].likes +=1
 
-  return response.json(repositories[repositoryIndex].likes)
+  return response.json(repositories[repositoryIndex])
 });
 
 module.exports = app;
